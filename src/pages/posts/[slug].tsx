de55ce01cd51222eb1e51c69e1c7a13017ec29a2
Marc <marcelopgm65@gmail.com>
2021-03-31 20:22:48 -0300

    :wrench: chore: Session type any to avoid error

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -39,10 +39,10 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = await getSession({ req });
+  const session: any = await getSession({ req });
   const { slug } = params;
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
